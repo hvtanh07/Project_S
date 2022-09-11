6d06423f93d0a671fa89c228891a670ae4645403
Tuấn Anh <32663098+hvtanh07@users.noreply.github.com>
2022-09-11 14:39:59 +0700

    minor changed

---
+++ ProjectSword/Assets/Scripts/BattleSystem/Cards/UnlockPoint.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockPoint : MonoBehaviour
+{
+    public bool unlocked;
+    public List<UnlockPoint> nextUnlockPoint;
+}
